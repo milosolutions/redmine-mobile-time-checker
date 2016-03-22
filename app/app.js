4bcd1aa704cf003853aaddd8bd2a9c83934dd4b1
wisedrette <ny4koo@gmail.com>
2016-03-22 23:26:52 +0200

    basic screens(pages) structure and flow between them

--- app/app.js
+++ app/app.js
@@ -1,96 +1,69 @@
-import {App, Platform} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
+import { App, Platform, IonicApp, Storage, LocalStorage, MenuController } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+import { LoginPage } from './pages/login/login.js';
+import { HomePage } from './pages/home/home.js';
+import { ReportPage } from './pages/report/report.js';
+import { SettingsPage } from './pages/settings/settings.js';
 import { Injectable, Injector } from 'angular2/core';
 import { RedmineApi } from './providers/redmine-api/redmine-api.js'
 
 @App({
-    template: '<ion-nav [root]="rootPage"></ion-nav>',
+    templateUrl: 'build/app.html',
     providers: [RedmineApi],
     config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
     static get parameters() {
-        return [[Platform], [RedmineApi]];
+        return [[IonicApp], [Platform], [RedmineApi], [MenuController]];
     }
 
-    constructor(platform,  _redmineService) {
-        this.rootPage = TabsPage;
+    constructor(app, platform,  _redmineService, menu) {
+        this.app = app;
         this.platform = platform;
+        this.menu = menu;
         this._redminer = _redmineService;
         this.initializeApp();
+
+        this.pages = [
+            { title: 'Home', component: HomePage },
+            { title: 'Redmine report', component: ReportPage },
+            { title: 'Settings', component: SettingsPage }
+        ];
+
+        this.local = new Storage(LocalStorage);
+        let hasKey = this.local.get('key')._result != null;
+        if (!hasKey) {
+            this.rootPage = LoginPage;
+        } else {
+            this.rootPage = HomePage;
+        }
     }
+
     initializeApp() {
         this.platform.ready().then(() => {
-            this._redminer.load();
-
+            // Okay, let's pass key parameter this way
+            let key = this.local.get('key')._result;
+            if (key != null) {
+                let nav = this.app.getComponent('nav');
+                nav.setRoot(HomePage, {
+                    key: key
+                });
+            }
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             StatusBar.styleDefault();
         });
     }
+
+    openPage(page) {
+        // close the menu when clicking a link from the menu
+        this.menu.close();
+        // navigate to the new page if it is not the current page
+        let nav = this.app.getComponent('nav');
+        if (page.title === 'Settings') {
+            nav.push(page.component);
+        } else {
+            nav.setRoot(page.component);
+        }
+    }
 }
-//import { App, IonicApp, Platform, MenuController } from 'ionic-angular';
-//import { HelloIonicPage } from './pages/hello-ionic/hello-ionic';
-//import { ListPage } from './pages/list/list';
-//
-//
-//@App({
-//    templateUrl: 'build/app.html',
-//
-//    config: {} // http://ionicframework.com/docs/v2/api/config/Config/
-//})
-//class MyApp {
-//    static get parameters() {
-//        return [[IonicApp], [Platform], [MenuController], [RedmineApi]];
-//    }
-//
-//    constructor(app, platform, menu, _redmineService) {
-//        // set up our app
-//        this._redminer = _redmineService;
-//        this.app = app;
-//        this.platform = platform;
-//        this.menu = menu;
-//        this.initializeApp();
-//
-//        // set our app's pages
-//        this.pages = [
-//            {title: 'Hello Ionic', component: HelloIonicPage},
-//            {title: 'My First List', component: ListPage}
-//        ];
-//
-//        // make HelloIonicPage the root (or first) page
-//        this.rootPage = HelloIonicPage;
-//    }
-//
-//    initializeApp() {
-//        this.platform.ready().then(() => {
-//            this._redminer.load();
-//
-//            // The platform is now ready. Note: if this callback fails to fire, follow
-//            // the Troubleshooting guide for a number of possible solutions:
-//            //
-//            // Okay, so the platform is ready and our plugins are available.
-//            // Here you can do any higher level native things you might need.
-//            //
-//            // First, let's hide the keyboard accessory bar (only works natively) since
-//            // that's a better default:
-//            //
-//            //
-//            // For example, we might change the StatusBar color. This one below is
-//            // good for light backgrounds and dark text;
-//
-//            if (window.StatusBar) {
-//                window.StatusBar.styleDefault();
-//            }
-//        });
-//    }
-//
-//    openPage(page) {
-//        // close the menu when clicking a link from the menu
-//        this.menu.close();
-//        // navigate to the new page if it is not the current page
-//        let nav = this.app.getComponent('nav');
-//        nav.setRoot(page.component);
-//    }
-//}
