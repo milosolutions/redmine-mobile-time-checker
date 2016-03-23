3b3bdc848eb2d607b2566e42ea46bb55d805ff4d
wisedrette <ny4koo@gmail.com>
2016-03-23 20:11:24 +0200

    basic alerts and settings functionality

--- app/pages/settings/settings.js
+++ app/pages/settings/settings.js
@@ -1,5 +1,6 @@
-import { Page, NavController, NavParams, Storage, LocalStorage } from 'ionic-angular';
+import { Page, NavController, NavParams, Alert, Storage, LocalStorage } from 'ionic-angular';
 import { HomePage } from '../home/home'
+import { runSettingsAlert } from '../../directives/helpers'
 
 @Page({
     templateUrl: 'build/pages/settings/settings.html'
@@ -11,9 +12,35 @@ export class SettingsPage {
 
     constructor(nav, navParams) {
         this.nav = nav;
+        this.local = new Storage(LocalStorage);
+        let key = this.local.get('key')._result;
+        let hours = this.local.get('hours_week')._result;
+        this.settings = [
+            {
+                title: 'I\'m on vacation!',
+                name: 'vacation',
+                message: 'Please specify end of vacation period'
+            },
+            {
+                title: 'Change working hours per week',
+                name: 'hours_week',
+                message: 'Current hours: ' + hours,
+                placeholder: 'Enter new working hours'
+            },
+            {
+                title: 'Update Redmine API key',
+                name: 'key',
+                message: 'Current key: ' + key,
+                placeholder: 'Enter new API key'
+            }
+        ];
     }
 
-    save(event){
+    save(event) {
         this.nav.pop();
+    }
+
+    update(setting) {
+        runSettingsAlert(setting, this.nav, this.local);
     }
 }
